ba5472d4a6fcbeceaba8abdc865a836098072fed
ms827 <min960827@gmail.com>
2023-03-07 20:55:58 +0900

    재귀함수

--- 이민석/29_9663.py
+++ 이민석/29_9663.py
@@ -4,80 +4,28 @@ st = time.time()  # 시작 시간 저장
 sys.stdin=open("이민석\input.txt", "rt")
 
 n = int(input())
-
+info = [0] * n
+located = [False] * n
+diag1 = [False] * (2 * n -1)
+diag2 = [False] * (2* n -1)
 ans = 0
-row = [0] * n
 
-def is_promising(x):
-    for i in range(x):
-        # 같은 열에 있는지 검사
-        if row[x] == row[i]:
-            return False
-        # 대각선 방향에 있는지 검사
-        if abs(row[x] - row[i]) == x - i:
-            return False
-    return True
-    
-
-def n_queens(x):
+def set(i):
     global ans
-    if x == n:
-        ans += 1
-        return
-
-    else:
-        for i in range(n):
-            # [x, i]에 퀸을 놓겠다.
-            row[x] = i
-            if is_promising(x):
-                n_queens(x+1)
-
-n_queens(0)
+    for j in range(n):
+        if not located[j] and not diag1[i+j] and not diag2[i-j+ n -1]:
+            info[i] = j
+            print(info)
+            if i == n - 1:
+                ans += 1
+            else:
+                located[j] = diag1[i+j] = diag2[i-j+ n -1] = True
+                print(located)
+                print(diag1)
+                print(diag2)
+                set(i+1)
+                located[j] = diag1[i+j] = diag2[i-j+ n -1] = False
+    
+set(0)
 print(ans)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-def n_queens (i, col):
-    n = len(col) - 1
-    if (promising(col, i)):
-        if (i == n):
-            print(col[1: n + 1])
-        else:
-            for j in range(1, n + 1):
-                col[i + 1] = j
-                n_queens(col, i + 1)
-
-def promising (i, col):
-    k = 1
-    flag = True
-    while (k < i and flag):
-        if (col[i] == col[k] or abs(col[i] - col[k]) == (i - k)):
-            flag = False
-        k += 1
-    return flag
-
-n = 8
-col = [0] * (n + 1)
-n_queens(0, col)
-"""
 print("time :", time.time() - st)
---
+++ 이민석/test.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.stdin=open("이민석\input.txt", "rt")
+
+N, r, c = map(int, input().split()) 
+def search(N, r, c): 
+    if N==0: 
+        return 0 
+    M=2**(N-1) 
+    if r<M and c<M: 
+        return search(N-1, r, c) 
+    elif r<M and M<=c: 
+        return M*M+search(N-1, r, c-M) 
+    elif M<=r and c<M: 
+        return 2*M*M+search(N-1, r-M, c) 
+    elif M<=r and M<=c: 
+        return 3*M*M+search(N-1, r-M, c-M) 
+print(search(N, r, c))
